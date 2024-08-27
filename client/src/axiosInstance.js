504b630060363ff9105cf4553771140d6580b839
AJIN_VR <ajinvr44@gmail.com>
2024-08-27 12:37:16 +0530

    loader  and error page addded updated

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:500',
   headers: {
     'Content-Type': 'application/json',
   },
--- client/src/pages/globalComponents/error/Errorpage.jsx
+++ client/src/pages/globalComponents/error/Errorpage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Errorpage() {
   return (
-    <div className="flex items-center justify-center bg-gray-100">
+    <div className="flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-red-500">500</h1>
-        <p className="mt-4 text-xl text-gray-700">Oops! Something went wrong on our end.</p>
+        <p className="mt-4 text-xl text-gray-600">Oops! Something went wrong on our end.</p>
         <p className="mt-2 text-gray-600">Please try again later.</p>
         <a
           href="/"
